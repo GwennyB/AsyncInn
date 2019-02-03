f2b883ca0c5e56a4dbb68e32740987feda3962e0
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-02-03 14:32:08 -0800

    added summary comments to all controllers

--- AsyncInn/Controllers/AmenityController.cs
+++ AsyncInn/Controllers/AmenityController.cs
@@ -15,12 +15,20 @@ namespace AsyncInn.Controllers
     {
         private readonly IAmenity _context;
 
+        /// <summary>
+        /// sets database context to _context field
+        /// </summary>
         public AmenityController(IAmenity context)
         {
             _context = context;
         }
 
-        // GET: Amenity
+        /// <summary>
+        /// GET: Amenity
+        /// gets all (filtered) rows and sends to Client in Index view
+        /// </summary>
+        /// <param name="searchString"> filtering criterion </param>
+        /// <returns> populated Index view </returns>
         public IActionResult Index(string searchString)
         {
             var searchResults = from roomPlan in _context.GetAmenities()
@@ -35,7 +43,12 @@ namespace AsyncInn.Controllers
             return View(_context.GetAmenities());
         }
 
-        // GET: Amenity/Details/5
+        /// <summary>
+        /// GET: Amenity/Details/5
+        /// gets row 'id' and sends to Client in Details view
+        /// </summary>
+        /// <param name="id"> ID of row to show </param>
+        /// <returns> populated Details view (or NotFound error view) </returns>
         public IActionResult Details(int? id)
         {
             if (id == null)
@@ -53,17 +66,23 @@ namespace AsyncInn.Controllers
             return View(amenity);
         }
 
-        // GET: Amenity/Create
+        /// <summary>
+        /// GET: Amenity/Create
+        /// loads Create page
+        /// </summary>
+        /// <returns> Create view </returns>
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Amenity/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// POST: Amenity/Create
+        /// adds 'amenity' to table as new row
+        /// </summary>
+        /// <param name="amenity"> amenity to add </param>
+        /// <returns> populated Index view with new row shown (or Delete view with 'amenity' details populated if model errors exist) </returns>
         [HttpPost]
-        
         public async Task<IActionResult> Create([Bind("ID,Description")] Amenity amenity)
         {
             if (ModelState.IsValid)
@@ -74,7 +93,12 @@ namespace AsyncInn.Controllers
             return View(amenity);
         }
 
-        // GET: Amenity/Edit/5
+        /// <summary>
+        /// GET: HoAmenitytel/Edit/5
+        /// gets a specific row from table 
+        /// </summary>
+        /// <param name="id"> ID of row to get </param>
+        /// <returns> Edit view with detail populated for row 'id' </returns>
         public IActionResult Edit(int? id)
         {
             if (id == null)
@@ -90,11 +114,14 @@ namespace AsyncInn.Controllers
             return View(amenity);
         }
 
-        // POST: Amenity/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// POST: Amenity/Edit/5
+        /// updates table with details in 'amenity'
+        /// </summary>
+        /// <param name="id"> ID of amenity to update </param>
+        /// <param name="amenity"> amenity details to use for update </param>
+        /// <returns>Index view populated with all records (including update), or NotFound error page (if 'id' not found), or Edit view with 'amenity' populated (if model errors exist)</returns>
         [HttpPost]
-        
         public async Task<IActionResult> Edit(int id, [Bind("ID,Description")] Amenity amenity)
         {
             if (id != amenity.ID)
@@ -124,7 +151,12 @@ namespace AsyncInn.Controllers
             return View(amenity);
         }
 
-        // GET: Amenity/Delete/5
+        /// <summary>
+        /// GET: Amenity/Delete/5
+        /// loads Delete confirmation page with details shown for 'id'
+        /// </summary>
+        /// <param name="id"> ID of row to delete </param>
+        /// <returns> 'Delete' confirmation page with 'id' loaded, or NotFound error if not found or none selected </returns>
         public IActionResult Delete(int? id)
         {
             if (id == null)
@@ -142,9 +174,13 @@ namespace AsyncInn.Controllers
             return View(amenity);
         }
 
-        // POST: Amenity/Delete/5
+        /// <summary>
+        /// POST: Amenity/Delete/5
+        /// deletes 'id' row from table
+        /// </summary>
+        /// <param name="id"> ID of row to delete </param>
+        /// <returns> Index view showing updated list of rows </returns>
         [HttpPost, ActionName("Delete")]
-        
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var amenity = _context.GetAmenities().FirstOrDefault(m => m.ID == id);
@@ -152,6 +188,11 @@ namespace AsyncInn.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        /// <summary>
+        /// HELPER: confirms existence of row by ID
+        /// </summary>
+        /// <param name="id"> ID of row to confirm </param>
+        /// <returns> true if present, false if not </returns>
         private bool AmenityExists(int id)
         {
             return _context.GetAmenities().Any(e => e.ID == id);
